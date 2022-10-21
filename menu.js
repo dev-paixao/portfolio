1f2d7559c73486704aa5582f6fb216495deec8d7
Diego Paixão <diego.spaixao91@hotmail.com>
2022-10-21 01:10:15 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,9 +2,23 @@ let show = true;
 
 const menuSection = document.querySelector(".cabecalho")
 const menuToggle = menuSection.querySelector(".menu-toggle")
+const homeSection = document.querySelector(".home")
 
 menuToggle.addEventListener("click", () => {
-    
+
     menuSection.classList.toggle("on", show)
     show = !show;
+
+    homeSection.classList.toggle("on", show)
+    show = !show;
+
+
+})
+
+menuToggle.addEventListener("click", () => {
+
+    homeSection.classList.toggle("on", show)
+    show = !show;
+
+
 })
