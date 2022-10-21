d37dd900bb226416ad7035cccef6bbf50dda4bcf
Diego Paixão <diego.spaixao91@hotmail.com>
2022-10-21 18:35:33 -0300

    Update menu responsivo

--- menu.js
+++ menu.js
@@ -1,24 +1,21 @@
-let show = true; 
+let show = true;
+let show1 = true; 
+let show2 = true;  
 
 const menuSection = document.querySelector(".cabecalho")
 const menuToggle = menuSection.querySelector(".menu-toggle")
 const homeSection = document.querySelector(".home")
+const logoSection = document.querySelector(".logo-responsive")
 
 menuToggle.addEventListener("click", () => {
 
     menuSection.classList.toggle("on", show)
     show = !show;
 
-    homeSection.classList.toggle("on", show)
-    show = !show;
+    homeSection.classList.toggle("on", show1)
+    show1 = !show2;
 
-
-})
-
-menuToggle.addEventListener("click", () => {
-
-    homeSection.classList.toggle("on", show)
-    show = !show;
-
+    logoSection.classList.toggle("off", show2)
+    show2 = !show2;
 
 })
