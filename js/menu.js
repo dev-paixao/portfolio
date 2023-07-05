101fabd921da4351e6a82cb283732c15d9f25517
Diego Paixão <34756441+dev-paixao@users.noreply.github.com>
2023-07-04 21:36:52 -0300

    novos projetos
    adição de novos cards de projetos para a seção "Portfolio".

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,13 @@ let menuVisible = true;
 let homeVisible = true;
 let logoVisible = true;
 
+// Seleciona os elementos do menu, home e logo
 const menuSection = document.querySelector(".header");
 const menuToggle = menuSection.querySelector(".menu-toggle");
 const homeSection = document.querySelector(".home");
 const logoSection = document.querySelector(".logo-responsive");
 
+// Adiciona um ouvinte de evento ao clique do botão de menu
 menuToggle.addEventListener("click", () => {
   // Alterna a visibilidade do menu e atualiza o estado
   menuSection.classList.toggle("on", menuVisible);
@@ -21,4 +23,3 @@ menuToggle.addEventListener("click", () => {
   logoSection.classList.toggle("off", logoVisible);
   logoVisible = !logoVisible;
 });
-
